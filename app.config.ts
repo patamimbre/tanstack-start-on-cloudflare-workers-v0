9c76e5acc934fd9683fc8840cd6ecf98ed866ff0
Matthew Sessions <matthewlsessions@gmail.com>
2025-04-18 20:58:48 -0600

    working start project on CF workers and local

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,22 @@
-import { defineConfig } from '@tanstack/react-start/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@tanstack/react-start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { cloudflare } from "unenv";
+import nitroCloudflareBindings from "nitro-cloudflare-dev";
 
 export default defineConfig({
+  server: {
+    preset: "cloudflare-module",
+    unenv: cloudflare,
+    modules: [nitroCloudflareBindings],
+  },
   tsr: {
-    appDirectory: 'src',
+    appDirectory: "src",
   },
   vite: {
     plugins: [
       tsConfigPaths({
-        projects: ['./tsconfig.json'],
+        projects: ["./tsconfig.json"],
       }),
     ],
   },
-})
+});
--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -1,19 +1,29 @@
-import { json } from '@tanstack/react-start'
-import { createAPIFileRoute } from '@tanstack/react-start/api'
-import type { User } from '../../utils/users'
+import { json } from "@tanstack/react-start";
+import { createAPIFileRoute } from "@tanstack/react-start/api";
+import { getBindings } from "~/utils/cf-bindings";
 
-export const APIRoute = createAPIFileRoute('/api/users')({
+export const APIRoute = createAPIFileRoute("/api/users")({
   GET: async ({ request }) => {
-    console.info('Fetching users... @', request.url)
-    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    console.info("Fetching users... @", request.url);
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
     if (!res.ok) {
-      throw new Error('Failed to fetch users')
+      throw new Error("Failed to fetch users");
     }
+    // Access CF bindings in API Route
+    const bindings = getBindings();
+    const deferredCount = await bindings.CACHE.get("queryCount");
 
-    const data = (await res.json()) as Array<User>
+    const data = (await res.json()) as Array<any>;
 
-    const list = data.slice(0, 10)
+    const list = data.slice(0, 10);
 
-    return json(list.map((u) => ({ id: u.id, name: u.name, email: u.email })))
+    return json(
+      list.map((u) => ({
+        id: u.id,
+        name: u.name,
+        email: u.email,
+        deferredCount,
+      })),
+    );
   },
-})
+});
